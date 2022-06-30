de6d0b9af0b240cbc27ee0865b2ac7ed4bfc0a7a
yaikohi <erik.beem@hotmail.com>
2022-06-30 14:28:06 +0200

    Initial commit, tailwindcss, prisma

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="flex items-center justify-center h-screen dark:bg-slate-900">
+      <h2 className="text-2xl font-extrabold dark:text-violet-300"> TailwindCSS is working </h2>
+    </div>
+  );
+}
