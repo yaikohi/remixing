cd403fb8846f12b5cb5bbdcdb317f546d0d565a7
yaikohi <erik.beem@hotmail.com>
2022-06-30 15:55:56 +0200

    WIP Add login page

---
+++ app/routes/login.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+    return (
+        <>
+            <div className="flex items-center justify-center h-screen">
+                <h2 className="text-2xl font-extrabold dark:text-violet-300">Login route</h2>
+            </div>
+        </>
+    )
+}
