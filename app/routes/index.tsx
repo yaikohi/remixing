fc30abf2bf92516d547dc57d1d5ea0c73d1a94f9
Erik Beem <erik.beem@hotmail.com>
2022-07-01 13:20:50 +0200

    Add auth guard on index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,12 @@
+// app/routes/index.tsx
+import type { LoaderFunction } from "@remix-run/node";
+import { requireUserId } from "~/utils/auth.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUserId(request);
+  return null;
+};
+
 export default function Index() {
   return (
     <div className="flex items-center justify-center h-screen bg-blue-100 dark:bg-slate-900">
