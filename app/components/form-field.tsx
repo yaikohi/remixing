e3710b9615355867a8d2b8eea808927eab570289
yaikohi <erik.beem@hotmail.com>
2022-07-01 11:32:16 +0200

    Add form-field component, update styling

---
+++ app/components/form-field.tsx
@@ -0,0 +1,19 @@
+interface FormFieldProps {
+    htmlFor: string,
+    label: string,
+    type?: string,
+    value: string,
+    onChange?: (...args: any) => any,
+}
+
+export function FormField({ htmlFor, label, type = 'text', value, onChange = () => { } }: FormFieldProps) {
+    return (
+        <>
+            <label htmlFor={htmlFor} className="font-semibold dark:text-violet-400">
+                {label}
+            </label>
+
+            <input className="w-full p-2 my-2 rounded-xl" onChange={onChange} id={htmlFor} name={htmlFor} value={value} type={type} />
+        </>
+    )
+}
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,6 +1,19 @@
+import { useState } from 'react'
+import { FormField } from '~/components/form-field'
 import { Layout } from '~/components/layout'
 
 export default function Login() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+    const [action, setAction] = useState('login')
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, field: string) => {
+        setFormData(form => ({ ...form, [field]: event.target.value }))
+    }
+
+
     return (
         <Layout>
             <div className="flex flex-col items-center justify-center h-full gap-y-4">
@@ -9,16 +22,23 @@ export default function Login() {
 
 
                 <form method="post" className="p-6 bg-gray-200 rounded-2xl w-96">
-                    <label htmlFor="email">Email</label>
-                    <input className="w-full p-2 my-2 rounded-xl" type="text" id="email" name="email" />
-
-                    <label htmlFor="password">Password</label>
-                    <input className="w-full p-2 my-2 rounded-xl" type="text" id="password" name="password" />
-
+                    <FormField
+                        htmlFor="email"
+                        label="Email"
+                        value={formData.email}
+                        onChange={e => handleInputChange(e, 'email')}
+                    />
+                    <FormField
+                        htmlFor="password"
+                        type="password"
+                        label="Password"
+                        value={formData.password}
+                        onChange={e => handleInputChange(e, 'password')}
+                    />
                     <div className="w-full text-center">
                         <input
                             type="submit"
-                            className="px-3 py-2 mt-2 font-semibold transition duration-300 ease-in-out bg-slate-300 text-violet-600 rounded-xl hover:bg-fuchsia-200 hover:-translate-y-1"
+                            className="px-3 py-2 mt-2 font-semibold text-blue-600 transition duration-300 ease-in-out bg-yellow-300 rounded-xl hover:bg-yellow-400 hover:-translate-y-1"
                             value="Sign In"
                         />
                     </div>
